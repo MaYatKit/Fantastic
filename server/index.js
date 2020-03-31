2599a2c785e720d3d036baa6cdd5d7b407387197
JimmyMa <ma417704684@gmail.com>
2020-03-31 16:55:42 +1300

    Fix all code style problems

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 
 // Setup Express
 const app = express();
 const port = process.env.PORT || 1000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => console.log(`App server listening on port ${port}!`));
