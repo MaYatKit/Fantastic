baf215cd72ff6976e00732e98de489f7b38b57ec
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-04-06 02:21:34 +1200

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Page from "./Components/page";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Page />
     </div>
   );
 }
---
+++ client/src/Components/button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./button.css";
+
+class Button extends React.Component{
+
+    render() {
+        if (this.props.pos != null) {
+            return (
+                <button className={`button ${this.props.type}`} onClick={() => this.props.changePagePosition(this.props.pos)}><span>{this.props.name}</span></button>
+            );
+        } else {
+            return (
+                <button className={`button ${this.props.type}`}><span>{this.props.name}</span></button>
+            );
+        }
+    }
+}
+
+export default Button;
