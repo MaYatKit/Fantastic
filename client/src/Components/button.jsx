96bddaa42936868ce6d569ae3346bad764975ad5
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-04-07 11:07:21 +1200

    fade in buttons

--- client/src/Components/button.jsx
+++ client/src/Components/button.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import "./button.css";
 
 class Button extends React.Component{
-
     render() {
         if (this.props.pos != null) {
             return (
-                <button className={`button ${this.props.type}`} onClick={() => this.props.changePagePosition(this.props.pos)}><span>{this.props.name}</span></button>
+                <button className={`button animateIn ${this.props.type}`} onClick={() => this.props.changePagePosition(this.props.pos)}><span>{this.props.name}</span></button>
             );
         } else {
             return (
