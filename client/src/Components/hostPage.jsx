0b4d4ae79da7e9006664a0cbac3f55f1e6db5565
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-04-16 00:56:17 +1200

    searching done needs styling

--- client/src/Components/hostPage.jsx
+++ client/src/Components/hostPage.jsx
@@ -1,11 +1,84 @@
 import React from 'react';
 import SearchBar from "./SearchBar";
 
+import './hostPage.css';
+
+const hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+        if (item) {
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+    }, {});
+
+window.location.hash = '';
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const clientId = '38c1268007c94332bec6779dadad7837';
+const redirectUri = 'http://localhost:3000';
+const scopes = [
+    'user-read-email',
+    'user-read-private',
+];
+
+if (!_token) {
+    window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+}
+
 class HostPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tracks: []
+        };
+
+        this.GetResult = this.GetResult.bind(this);
+    }
+
+    GetResult(searchItem) {
+        if (searchItem) {
+            fetch("https://api.spotify.com/v1/search?q=" + searchItem + "&type=track&market=NZ", {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + _token
+                }
+            }).then(data => {
+                fetch(data.url)
+                    .then(response => response.json())
+                    .then((jsonData) => {
+                        this.setState({tracks: []});
+                        for (let i = 0; i < jsonData.tracks.items.length; i++) {
+                            this.setState({tracks: [...this.state.tracks, {trackName: jsonData.tracks.items[i].name, albumName: jsonData.tracks.items[i].album.name, artistName: jsonData.tracks.items[i].artists[0].name}]});
+                        }
+                    });
+            });
+        } else {
+            this.setState({tracks: []});
+        }
+    }
+
     render() {
+        console.log(this.state.tracks);
         return (
             <div>
-                <SearchBar />
+                <SearchBar GetResult={this.GetResult}/>
+                <div>
+                    {this.state.tracks.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <h2>{item.trackName}</h2>
+                                <h6>{item.albumName} {item.artistName}</h6>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         );
     }
