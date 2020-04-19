c46392293748c0dc5eaa72dd36f6a2b0bdc7fde6
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-04-19 17:46:47 +1200

    manually uploading my changes

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -11,14 +11,19 @@ export default class SearchBar extends React.Component {
 
         this.state = {
             searching: false
-          };
+        };
     }
 
     handleOnChange(event){
         if(event.target.value){
-            this.setState({searching: true})
+            this.setState({searching: true});
         }else{
-            this.setState({searching: false})
+            this.setState({searching: false});
+        }
+        if (event.target.value === " ") {
+            this.props.GetResult("%20");
+        }else{
+            this.props.GetResult(event.target.value);
         }
     }
 
@@ -32,14 +37,16 @@ export default class SearchBar extends React.Component {
         }
 
         return(
-            <form className="form-wrapper">
-                { icon }
-                <input className = "input-field"
-                type = "text"
-                placeholder = "Add Tracks" 
-                onChange = {event => this.handleOnChange(event)}
-                />
-            </form>
+            <div>
+                <form className="form-wrapper">
+                    <div className={"image"}>{ icon }</div>
+                    <input className = "input-field"
+                    type = "text"
+                    placeholder = "Add Tracks"
+                    onChange = {event => this.handleOnChange(event)}
+                    />
+                </form>
+            </div>
         );
     }
 
---
+++ client/src/components/landingPage.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Button from "./button";
+import "./landingPage.css";
+
+import {FaArrowLeft} from "react-icons/fa";
+import logo from "./../image/logo.png"
+
+class LandingPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            pagePosition: 0
+        };
+
+        this.changePagePosition = this.changePagePosition.bind(this);
+    }
+
+    changePagePosition(pos) {
+        this.setState({pagePosition: pos});
+    }
+
+    render() {
+        switch (this.state.pagePosition) {
+            case 0:
+                return (
+                    <div className={"landingPage"}>
+                        <img className={"logo"} src={logo} alt={"logo"} />
+                        <div className={"landing"}>
+                            <Button name={"CREATE PARTY"} type={"main"} pos={1} changePagePosition={this.changePagePosition} />
+                            <Button name={"JOIN PARTY"} type={"main"} pos={2} changePagePosition={this.changePagePosition} />
+                        </div>
+                    </div>
+                );
+            case 1:
+                return (
+                    <div className={"landingPage"}>
+                        <img className={"logo"} src={logo} alt={"logo"} />
+                        <div className={"creating"}>
+                            <Button name={"clicked"} type={"main"} pos={0} changePagePosition={this.changePagePosition}/>
+                            <Button name={"JOIN PARTY"} type={"main"} pos={0} changePagePosition={this.changePagePosition} />
+                        </div>
+                    </div>
+                );
+            case 2:
+                return (
+                    <div className={"landingPage"}>
+                        <img className={"logo"} src={logo} alt={"logo"} />
+                        <div className={"joining"}>
+                            <div className={"inputArea"}>
+                                <button className={"arrow"} onClick={() => this.changePagePosition(0)}><FaArrowLeft/></button>
+                                <input className={"codeInput"} type={"text"} placeholder={"ENTER CODE"} />
+                            </div>
+                            <Button name={"JOIN"} type={"main"} />
+                        </div>
+                    </div>
+                );
+            default:
+                break;
+        }
+    }
+}
+
+export default LandingPage;
