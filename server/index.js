a42d2a2ff4168a22bc0a96cb4be81b952a25f305
JimmyMa <ma417704684@gmail.com>
2020-04-21 18:05:07 +1200

    1. change config to allow cross domain request 2. use cookies to identify user

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const cors = require('cors')
 require("./src/config/passport-setup")
+var cookieParser = require('cookie-parser')
 
 // Setup Express
 const app = express();
@@ -20,9 +21,24 @@ db.once('open', function(){
 });
 
 app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(express.json())
-app.use(passport.initialize())
+// app.use(cors({
+//     origin:["http://localhost:3000"],
+//     methods:['GET','POST', 'PUT'],
+//     alloweHeaders:['Content-Type', 'Authorization','Accept'],
+//     allowCredentials:true
+// }))
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+};
+app.use(allowCrossDomain);
+
+app.use(express.json());
+app.use(passport.initialize());
+app.use(cookieParser());
 
 //Exporting routes
 const auth = require("./src/routes/auth-route")
