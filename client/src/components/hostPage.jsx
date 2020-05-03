f3045ea331c41dd8fc4491e011d4657a391d1efe
JimmyMa <ma417704684@gmail.com>
2020-05-03 14:48:29 +1200

    1. finish logic of the search function

--- client/src/components/hostPage.jsx
+++ client/src/components/hostPage.jsx
@@ -4,9 +4,10 @@ import './hostPage.css';
 import SideBar from './SideBar';
 import MusicLi from './MusicLi';
 import { connect } from 'react-redux';
-import api from '../api'
-import { MdAdd } from "react-icons/md";
-import { MdCheck } from "react-icons/md";
+import api from '../api';
+import { MdAdd } from 'react-icons/md';
+import { MdCheck } from 'react-icons/md';
+import { refreshPlaylist } from '../redux/actions';
 
 
 class ConnectHostPage extends React.Component {
@@ -23,39 +24,72 @@ class ConnectHostPage extends React.Component {
 
         this.GetResult = this.GetResult.bind(this);
         this.searchRef = React.createRef();
-        // this.checkState = this.checkState.bind(this);
-        // this.checkState();
+        this.savePlaylist = this.savePlaylist.bind(this);
     }
 
-    checkState() {
-        if (this.state.musicInfo === undefined) {
-            // after refresh page, need to query data from sever again
+    savePlaylist() {
+        if (this.state.musicInfo.length !== 0) {
+            this.props.refreshPlaylist(this.state.musicInfo);
         }
     }
 
     GetResult(searchItem) {
         if (searchItem) {
             api.searchItem(searchItem)
-            .then(array => {
-                if (this.searchRef.current.state.searching){
-                    console.log("search outcome: ", array);
-                    this.setState({
-                        tracks: array,
-                        active: true
-                    });
-                }
-            })
+                .then(array => {
+                    if (this.searchRef.current.state.searching) {
+                        console.log('search outcome: ', array);
+                        this.setState({
+                            tracks: array,
+                            active: true
+                        });
+                    }
+                });
         } else {
             this.setState({ tracks: [] });
             this.setState({ active: false });
         }
     }
 
-    componentDidMount(){
-        api.isLogin().catch(obj => {
-            alert("you need log in to see this page")
-            api.login();
-        })
+    componentDidMount() {
+        api.isLogin()
+            .catch(obj => {
+                alert('you need log in to see this page');
+                api.login();
+            });
+    }
+
+
+    selectSearchItem(item) {
+        if (item.selected) {
+            item.selected = false;
+            for (let i = 0; i < this.state.musicInfo.length; i++) {
+                if (this.state.musicInfo[i]['_id'] === item['id']) {
+                    this.state.musicInfo.splice(i, 1);
+                    break;
+                }
+            }
+
+        } else {
+            item.selected = true;
+            this.state.musicInfo[this.state.musicInfo.length] = {
+                'votes': 1,
+                '_id': item['id'],
+                'name': item['trackName'],
+                'uri': item['uri'],
+                'artist': item['artistName'],
+                'album': item['albumName'],
+                'albumIcon': {
+                    'small': item.albumIcon['small'].url,
+                    'large': item.albumIcon['large'].url
+                }
+            };
+        }
+        this.setState({
+            tracks: this.state.tracks,
+            active: true,
+            musicInfo: this.state.musicInfo
+        });
     }
 
     render() {
@@ -63,12 +97,20 @@ class ConnectHostPage extends React.Component {
             <div className={'hostPage'}>
                 <SideBar userName={this.state.userName} roomId={this.state.roomId}> </SideBar>
                 <div style={{ marginLeft: '260px' }}>
-                    <SearchBar GetResult={this.GetResult} ref = {this.searchRef}/>
+                    <SearchBar GetResult={this.GetResult} ref={this.searchRef}/>
                     <div className={'page'}>
-                        <div className={'search-results ' + (this.state.active ? '' : 'hidden')}>
+                        <div className={'search-results ' + (this.state.active ? '' : 'hidden')}
+                             onClick={() => {
+                                 this.setState({ active: false });
+                                 this.searchRef.current.state.searching = false;
+                                 this.savePlaylist();
+                             }}>
                             {this.state.tracks.map((item, index) => {
                                 return (
-                                    <div className={'result'} key={index}>
+                                    <div className={'result'} key={index} onClick={event => {
+                                        this.selectSearchItem(item);
+                                        event.stopPropagation();
+                                    }}>
                                         <div className="img">
                                             <img src={item.albumArt} alt=""/>
                                         </div>
@@ -86,15 +128,14 @@ class ConnectHostPage extends React.Component {
                                                 </span>
                                             </div>
                                         </div>
-                                        <div className={'addSong'} onClick={() => {
-                                            item.selected = true;
-                                            this.setState({
-                                                tracks: this.state.tracks,
-                                                active: true
-                                            });
+                                        <div className={'addSong'} onClick={event => {
+                                            this.selectSearchItem(item);
+                                            event.stopPropagation();
                                         }}>
-                                            {item.selected? <MdCheck className={'icon'}> </MdCheck> : <MdAdd className={'icon'}>
-                                            </MdAdd>}
+                                            {item.selected ?
+                                                <MdCheck className={'icon'}> </MdCheck> :
+                                                <MdAdd className={'icon'}>
+                                                </MdAdd>}
                                         </div>
 
                                     </div>
@@ -102,17 +143,19 @@ class ConnectHostPage extends React.Component {
                             })}
                         </div>
                         <div className={'tracklist'}>
-                            {this.state.musicInfo[0].map((entry, index) => {
-                                return (
-                                    <MusicLi name={entry.name}
-                                             album={entry.album}
-                                             votes={entry.votes}
-                                             icon={entry.albumIcon['large']}
-                                             index={index}
-                                             key={index}>
-                                    </MusicLi>
-                                );
-                            })}
+                            {
+                                this.state.musicInfo.length !== 0 ? this.state.musicInfo.map((entry, index) => {
+                                    return (
+                                        <MusicLi name={entry.name}
+                                                 album={entry.album}
+                                                 votes={entry.votes}
+                                                 icon={entry.albumIcon['large']}
+                                                 index={index}
+                                                 key={index}>
+                                        </MusicLi>
+                                    );
+                                }) : <span className={'hint'}> Search to add musics </span>
+                            }
                         </div>
                     </div>
                 </div>
@@ -127,12 +170,18 @@ const mapStateToProps = (state, ownProps) => {
         roomId: state.roomId,
 
         // existing playlist
-        musicInfo: state.musicInfo
+        musicInfo: state.musicInfo ? state.musicInfo : []
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        refreshPlaylist: data => dispatch(refreshPlaylist(data))
     };
 };
 
 
-export const HostPage = connect(mapStateToProps)(ConnectHostPage);
+export const HostPage = connect(mapStateToProps, mapDispatchToProps)(ConnectHostPage);
 
 
 // export default HostPage;
