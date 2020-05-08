d02be6a79bdb7a6945426cecb670701ad28df366
JimmyMa <ma417704684@gmail.com>
2020-05-08 12:40:30 +1200

    1. fix a problem of can not save status after refresh on host page 2. pass playlist changes to the backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,26 +1,27 @@
-const prefix = 'http://localhost:1000'
+const querystring = require('querystring');
+const prefix = 'http://localhost:1000';
 
-function get(url){
+function get(url) {
     // old XMLHttpRequest method for GET
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.withCredentials = true;
-        xhr.onload = ()=>{
+        xhr.onload = () => {
             resolve(xhr.responseText);
         };
-        xhr.onerror = ()=>{
+        xhr.onerror = () => {
             reject();
-        }
+        };
         xhr.send();
-    })
+    });
 }
 
-function login(){
-    window.location.replace(prefix + '/auth/spotify')
+function login() {
+    window.location.replace(prefix + '/auth/spotify');
 }
 
-function isLogin(){
+function isLogin() {
     return new Promise((resolve, reject) => {
         // return Promise
         // promise resolve when user is logged in
@@ -31,80 +32,117 @@ function isLogin(){
             mode: 'cors',
             credentials: 'include',
             headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json;charset=utf-8"
-        }})
-        .then(response => {
-            return response.json()
+                Accept: 'application/json',
+                'Content-Type': 'application/json;charset=utf-8'
+            }
         })
-        .then(obj => {
-            if (!Array.isArray(obj))
-                reject({
-                    message: 'server responded: not login',
-                    response: obj
-                })
-            else
-                resolve(obj)
+            .then(response => {
+                return response.json();
+            })
+            .then(obj => {
+                if (!Array.isArray(obj)) {
+                    reject({
+                        message: 'server responded: not login',
+                        response: obj
+                    });
+                } else {
+                    resolve(obj);
+                }
 
-        })
-    })
+            });
+    });
 
 }
 
-function createParty(){
+function createParty() {
     return fetch('http://localhost:1000/party/', {
         method: 'GET',
         mode: 'cors',
         credentials: 'include',
         headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=utf-8"
+            Accept: 'application/json',
+            'Content-Type': 'application/json;charset=utf-8'
         },
-    })
+    });
 }
 
-function searchItem(searchItem){
+function searchItem(searchItem) {
     // search music by keyword
     // return Promise, The promise return a array of music when resolved
 
-    let url = 'http://localhost:1000/search?q=' + searchItem + '&type=track&limit=10'
+    let url = 'http://localhost:1000/search?q=' + searchItem + '&type=track&limit=10';
     return fetch(url, {
-                method: 'GET',
-                mode: 'cors',
-                credentials: 'include',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json;charset=utf-8'
-                }
-    })
-    .then(response => {
-        return response.json()
-    })
-    .then(obj=>{
-        if (!obj.tracks){
-            console.error("need log in");
-            return []
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json;charset=utf-8'
         }
-
-        let goodArray = obj.tracks.items.map(e => {
-            return {
-                id : e.id,
-                uri: e.uri,
-                albumIcon:{
-                    "small": e.album.images[2],
-                    "large": e.album.images[1]
-                },
-                trackName: e.name,
-                albumName: e.album.name,
-                artistName: e.artists[0].name,
-                albumArt: e.album.images[0].url,
-                selected: false
-            }
-        })
-
-        return goodArray
     })
+        .then(response => {
+            return response.json();
+        })
+        .then(obj => {
+            if (!obj.tracks) {
+                console.error('need log in');
+                return [];
+            }
+
+            let goodArray = obj.tracks.items.map(e => {
+                return {
+                    id: e.id,
+                    uri: e.uri,
+                    albumIcon: {
+                        'small': e.album.images[2],
+                        'large': e.album.images[1]
+                    },
+                    trackName: e.name,
+                    albumName: e.album.name,
+                    artistName: e.artists[0].name,
+                    albumArt: e.album.images[0].url,
+                    selected: false
+                };
+            });
+
+            return goodArray;
+        });
+}
+
+function uploadPlayList(roomId, playList) {
+    // search music by keyword
+    // return Promise, The promise return a array of music when resolved
+
+    let url = 'http://localhost:1000/party/';
+
+    let data = {
+        id: roomId,
+        tracks: playList
+    };
+
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response['status'] === 200) {
+                console.log('Success to upload playlist!!');
+            } else {
+                console.log('Fail to upload playList!!! status code = ' + response['status']);
+            }
+        });
 }
 
 
-export default {login, isLogin, createParty, searchItem }
+export default {
+    login,
+    isLogin,
+    createParty,
+    searchItem,
+    uploadPlayList
+}
