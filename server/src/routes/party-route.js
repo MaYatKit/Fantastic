9470c56c797c3864191909b9b36c054a9f8121f1
JimmyMa <ma417704684@gmail.com>
2020-05-11 13:40:21 +1200

    1. fix a problem about crash on landing page when there is no tracks belong to a party 2. fix a problem about crash when refresh the host page

--- server/src/routes/party-route.js
+++ server/src/routes/party-route.js
@@ -6,18 +6,18 @@ router.get("/", async (req,res,next)=>{
     try{
         let partyDetails = await Host.aggregate([
             { "$match": { id: req.cookies.user} },
-            { "$group": {_id: null,
-                id: {"$first": "$party.id"},
+            { "$group": {_id: "$party.id",
+                room_id: {"$first": "$party.id"},
                 name: {"$first": "$name"},
-                tracks: {"$push": "$party.tracks"}}
+                tracks: {"$first": "$party.tracks"}}
             },
-            { "$project": {id: 1, tracks: 1 ,name: 1, _id: 0} }
+            { "$project": {room_id: 1, tracks: 1 ,name: 1, _id: 0} }
         ])
 
         if(!partyDetails || partyDetails.length === 0){
             res.status(404).json({message: "couldn't find host"})
         }else{
-            res.status(200).json(partyDetails)
+            res.status(200).json(partyDetails[0])
             res.end();
         }
 
